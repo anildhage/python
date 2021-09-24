c622f58da883205685b05ef483041d057fb6e691
anildhage <i.am.dhage@gmail.com>
2021-09-24 18:51:23 +0530

    comments placing changed

--- Data_structures&algorithms/bubble_sort.py
+++ Data_structures&algorithms/bubble_sort.py
@@ -1,18 +1,25 @@
-# here we implement bubble sort function. Example: How is a list sorted under the hood.
+# here we implement the bubble sort function. Example: How is a list sorted under the hood.
 
 def bubble_sort(array):
-        for i in range(len(array)): # loop through the range
-            for j in range(0,len(array) - i - 1): # loop until the range minus the last index
-                if array[j] > array[j+1]: # checks index 1 greater than index 2 
-                    temp = array[j] # copies the value to a new variable
-                    array[j] = array[j+1] # index 2 is placed in index 1
-                    array[j+1] = temp # index 1 is index 2
+        # loop through the range
+        for i in range(len(array)):
+            # loop until the range minus the last index
+            for j in range(0,len(array) - i - 1):
+                # checks index 1 greater than index 2 
+                if array[j] > array[j+1]:
+                    # copies the value to a new variable
+                    temp = array[j]
+                    # index 2 is placed in index 1
+                    array[j] = array[j+1]
+                    # index 1 is index 2
+                    array[j+1] = temp
 
 
 
 numbers = [99, 44, 6,2,1,5,63,87, 283,4,0]
 bubble_sort(numbers)
 print(numbers)
+
 
 
 #Tip: Before writing code, you need to understand how the execution will be.
